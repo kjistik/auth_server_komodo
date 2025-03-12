65ad12a3d56deea39be801405df0b8c02ab66b18
kjistik <coronelmartinernesto@gmail.com>
2025-03-12 02:03:54 -0300

    added logs for refresh token rotation

--- src/main/java/kjistik/auth_server_komodo/Utils/JwtUtils.java
+++ src/main/java/kjistik/auth_server_komodo/Utils/JwtUtils.java
@@ -50,16 +50,19 @@ public class JwtUtils {
         // Delete old refresh token, store new refresh token, and generate a new JWT
         // token
         return service.deleteRefreshToken(username)
-                .then(service.storeRefreshToken(username, generateRefreshToken(username)))
-                .then(Mono.fromCallable(() -> {
-                    return Jwts.builder()
-                            .subject(username)
-                            .issuedAt(new Date())
-                            .claim("roles", roles)
-                            .expiration(new Date(System.currentTimeMillis() + jwtConfig.getExpirationTime()))
-                            .signWith(getSecretKey())
-                            .compact();
-                }));
+                .doOnTerminate(() -> System.out.println("Deleting old refresh token for user: " + username))
+                .then(service.storeRefreshToken(username, generateRefreshToken(username))
+                        .doOnTerminate(() -> System.out.println("Storing new refresh token for user: " + username))
+                        .then(Mono.fromCallable(() -> {
+                            System.out.println("Generating JWT for user: " + username);
+                            return Jwts.builder()
+                                    .subject(username)
+                                    .issuedAt(new Date())
+                                    .claim("roles", roles)
+                                    .expiration(new Date(System.currentTimeMillis() + jwtConfig.getExpirationTime()))
+                                    .signWith(getSecretKey())
+                                    .compact();
+                        })));
     }
 
     public String generateRefreshToken(String username) {
