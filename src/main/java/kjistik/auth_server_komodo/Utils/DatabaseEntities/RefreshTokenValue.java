c538fc27e2518b4732eded2cf14ce90663da7738
kjistik <coronelmartinernesto@gmail.com>
2025-03-26 19:21:50 -0300

    added serializing support

--- src/main/java/kjistik/auth_server_komodo/Utils/DatabaseEntities/RefreshTokenValue.java
+++ src/main/java/kjistik/auth_server_komodo/Utils/DatabaseEntities/RefreshTokenValue.java
@@ -1,13 +1,20 @@
 package kjistik.auth_server_komodo.Utils.DatabaseEntities;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 
 @Getter
-public class RefreshTokenValue {
-    public String token;
-    public String fingerprint;
 
-    public RefreshTokenValue(String token, String fingerprint) {
+public class RefreshTokenValue {
+    private final String token;
+    private final String fingerprint;
+
+    @JsonCreator
+    public RefreshTokenValue(
+            @JsonProperty("token") String token,
+            @JsonProperty("fingerprint") String fingerprint) {
         this.token = token;
         this.fingerprint = fingerprint;
     }
